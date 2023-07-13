85ebe25f4309ede236b4be8f79a7bd7677632b91
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-07-13 20:54:21 +0300

    eligibility check debug

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,30 @@
 import  { useState ,useEffect } from 'react';
 import { db } from '@vercel/postgres';
 async function Form({username}) {
-    const { rows } = await db`SELECT * from users where account=${username}`;
+    const { rows } = await db`SELECT * from Users WHERE account=${username}`;
+
+    console.log(rows ? rows : "there is no rows");
+    console.log(username ? username : "there is no username");
+
+  return (
+  //  <div>dis {user ? user.account : username } is {user ? (user.eligible ? "eligible": "not eligible"): username } </div>
+
+  <div>
+  {rows.map((row) => (
+    <div key={row.id}>
+      {row.id} - {row.account}
+    </div>
+  ))}
+</div>
+
+
+  )
+}
+
+export default Form
+
+/*
+
 
     const [user, setUser] = useState({
         account:"",
@@ -18,10 +41,7 @@ async function Form({username}) {
       }, [username]);
     }
 
-  return (
-    <div>dis {user ? user.account : username } is {user ? (user.eligible ? "eligible": "not eligible"): username } </div>
-  )
-}
 
-export default Form
 
+
+*/
