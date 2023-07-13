35e2acc86ab554e2166545374453d6a48921920b
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-07-13 20:17:54 +0300

    eligibility checkk before

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,7 @@
-import { sql } from "@vercel/postgres";
-
 import  { useState ,useEffect } from 'react';
 
-async function Form({username}) {
-    const { rows } = await sql`SELECT * from Users where =${username}`;
+function Form({username}) {
+
     const [user, setUser] = useState({
         account:"",
         firstinvite:"",
@@ -12,18 +10,13 @@ async function Form({username}) {
         eligible: false
       });
 
-     if(rows[0].account===username){
-        useEffect(() => {
-            setUser({ ...user, account:  `@${username}`, eligible: true })
-          }, []);
-     }
+      useEffect(() => {
+        setUser({ ...user, account:  username  })
+      }, []);
  
 
   return (
-    <div>
-    <h1>
-    dis {user.account}  is {user.eligible ? "eligible" : "not eligible"}</h1>
-    </div>
+    <div>dis {user.account}</div>
   )
 }
 
