c65258502aad66300cbcd0f6a7a8e09310af1d40
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-07-13 13:08:38 +0300

    updating v1 auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ const handler= NextAuth({
         TwitterProvider({
           clientId: process.env.TWITTER_CLIENT_ID,
           clientSecret: process.env.TWITTER_CLIENT_SECRET,
-          version: "2.0",
+        
         }),
         GithubProvider({
             clientId: process.env.GITHUB_ID,
