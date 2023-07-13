68c7c27f77a265634d8a0d9f935e497744312747
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-07-13 19:46:55 +0300

    diff approach to state

---
+++ components/Form.jsx
@@ -0,0 +1,23 @@
+import  { useState ,useEffect } from 'react';
+
+function Form({username}) {
+
+    const [user, setUser] = useState({
+        account:"",
+        firstinvite:"",
+        secondinvite:"",
+        address:"",
+        eligible: false
+      });
+
+      useEffect(() => {
+        setUser({ ...user, account:  username  })
+      }, []);
+ 
+
+  return (
+    <div>dis {user.account}</div>
+  )
+}
+
+export default Form
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,46 +1,34 @@
 "use client";
-import  { useState ,useEffect } from 'react';
+
+import Form from "./Form";
 
 import { useSession } from "next-auth/react"
 function Header() {
   const {data:session,status } = useSession()
 
-  const [user, setUser] = useState({
-    account:"",
-    firstinvite:"",
-    secondinvite:"",
-    address:"",
-    eligible: false
-  });
 
 
-
-
-useEffect(() => {
+if(status==="authenticated"){
   
-    if(status==="authenticated"){
-      setUser({ ...user, account: session.user.name  })
-      console.log(user ? user :"user can't be found")
-      return(
-        <div>helloo <b>{session.user.name}  </b>you've succesffuly logged in and seeing protected content! 
-        
-        
-        </div>
-      );
-    }else{
- 
+  
+  return(
+    <div>helloo <b>{session.user.name}  </b>you've succesffuly logged in and seeing protected content! 
+    
+    <Form username={session.user.name} />
+    </div>
+  );
+}
 
 
-      return (
-      
-        <div>
-          youneed to sign in to see this page {user.account}
-        </div>
-      )
 
-    }
-  }, [session]);
 
+  
+  return (
+  
+    <div>
+      youneed to sign in to see this page {user.account}
+    </div>
+  )
 }
 
 export default Header
