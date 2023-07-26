ac8b04934705fb8623f0d90d0f16e8a3a14797d4
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-07-26 18:36:43 +0300

    trying new ways to get account name 11- debug

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,27 +10,18 @@ const handler = NextAuth({
       version: "2.0",
       callbacks: {
         async session(session, user) {
-          console.log('session callback triggered');
-          // Fetch additional user data using Twitter API or any other source
-          const additionalData = await fetchUserData(user.accessToken);
-    
-          // Merge the additional data into the session object
-          session.user = { ...session.user, ...additionalData };
+          session.user = {
+            ...session.user,
+            customField1: 'Hello, this is customField1!',
+            customField2: 42,
+            // Add more hardcoded data as needed
+          };
+
+          
+         
           return session;
         },
-      },
-      async jwt(token, user) {
-        // If user exists, it means the JWT is being generated on the server-side
-        if (user) {
-          // Add custom data to the JWT payload
-          token.debugData = {
-            key1: 'value1',
-            key2: 'value2',
-            // Add more custom data as needed
-          };
-        }
-        return token;
-      },
+      }
   
       
     }),
@@ -42,3 +33,14 @@ const handler = NextAuth({
 });
 
 export { handler as GET, handler as POST };
+
+/*
+
+ console.log('session callback triggered');
+          // Fetch additional user data using Twitter API or any other source
+          const additionalData = await fetchUserData(user.accessToken);
+    
+          // Merge the additional data into the session object
+          session.user = { ...session.user, ...additionalData };
+
+*/
