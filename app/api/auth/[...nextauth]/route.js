c4b7fe2154fd288e4e85918b765bdc3c0d9a2c5c
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-07-26 19:52:28 +0300

    trying new ways to get account name 17- debug

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,10 +11,13 @@ const handler = NextAuth({
       callbacks: {
         async session({ session, token, user }) {
           // Send properties to the client, like an access_token and user id from a provider.
-          session.accessToken = token.accessToken
-          session.user.id = token.id
-          
+         if(session && token && user){
+          session.accessToken = "token.accessToken"
+          session.user.id = "token.id"
           return session
+         }
+      
+         
         }
       }
     }),
