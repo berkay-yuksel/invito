e25affdbb71603c1d9520dfb308a4693ddde31f5
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-07-26 17:16:28 +0300

    trying new ways to get account name 3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,13 +8,7 @@ const handler = NextAuth({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
       version: "2.0",
-      callbacks: {
-        signIn: async (user, account, profile) => {
-          if (user) {
-            return account;
-          }
-        },
-      },
+      
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
